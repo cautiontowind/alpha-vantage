b31a2a0664d541f332cd6eb37d7b76ff5cde6650
John Doe <amirsalous@wattopoststudio.com>
2017-10-19 18:34:41 -0400

    Renaming Packages for reals

--- src/main/java/com/alpha_vantage/api/exception/MalFormattedFunctionException.java
+++
@@ -1,7 +0,0 @@
-package com.alpha_vantage.api.exception;
-
-public class MalFormattedFunctionException extends Exception{
-    public MalFormattedFunctionException(String function){
-        super("This API function ("+function+") does not exist.");
-    }
-}
--- src/main/java/com/alpha_vantage/api/exception/MissingApiKeyException.java
+++
@@ -1,7 +0,0 @@
-package com.alpha_vantage.api.exception;
-
-public class MissingApiKeyException extends Exception {
-    public MissingApiKeyException(){
-        super("the parameter apikey is invalid or missing. Please claim your free API key on (https://www.alphavantage.co/support/#api-key). It should take less than 20 seconds, and is free permanently.");
-    }
-}
